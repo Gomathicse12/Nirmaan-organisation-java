114d6778e978b08b82aec8699c39e3e4821578d1
Gomathicse12 <132119074+Gomathicse12@users.noreply.github.com>
2024-11-22 14:06:01 +0530

    Add files via upload

---
+++ OperatorTask.java
@@ -0,0 +1,50 @@
+package day1;
+
+public class OperatorTask {
+public static void main (String args[]) {
+	int firstNum =12;
+	int secondNum=25;
+	int thirdNum=10;
+	int fourthNum=50;
+	System.out.println("Arithmetic Operator:");
+	int addition=firstNum+secondNum;
+	int sub=firstNum-secondNum;
+	int mul=firstNum*secondNum;
+	int division=firstNum/secondNum;
+	int modulus=firstNum%secondNum;
+	
+	
+    System.out.println("add="+addition);
+    System.out.println("sub=" +sub);
+    System.out.println("mul="+mul);
+    System.out.println("div="+division);
+    System.out.println("mod="+modulus);
+   
+ 
+    System.out.println(" Relational Operators:");
+    boolean G =firstNum<secondNum;
+    boolean S =firstNum>secondNum;
+    boolean GE =firstNum<=secondNum;
+    boolean SE=firstNum<=secondNum;
+    boolean E =firstNum==secondNum;
+    boolean  M = firstNum!=secondNum;
+    
+    System.out.println("12<25: G");
+    System.out.println("12>25: S");
+    System.out.println("12<=25: GE");
+    System.out.println("12=>25: SE");
+    System.out.println("12==25: E");
+    System.out.println("12!=25: M");
+    
+    System.out.println(" Logical Operators:");
+    boolean A =thirdNum>fourthNum;
+    boolean B =thirdNum<fourthNum;
+    boolean C =thirdNum<fourthNum;
+    boolean D =thirdNum<fourthNum;
+    System.out.println(12>10: A)
+    
+    
+	
+	
+}
+}
