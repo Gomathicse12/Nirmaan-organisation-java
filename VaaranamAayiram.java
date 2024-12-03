eee5de68fad04ca9eddaa01dec11d9970a965ad6
Gomathicse12 <132119074+Gomathicse12@users.noreply.github.com>
2024-12-03 13:11:08 +0530

    day7

---
+++ VaaranamAayiram.java
@@ -0,0 +1,31 @@
+package day7;
+
+//import java.util.Scanner;
+
+public class VaaranamAayiram {
+	public static void main(String args[]) {
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter status");
+//		String megana=sc.nextLine();
+//		String status=new String("dead");
+//		if (megana.equals(status)) {
+//			System.out.println( "suriya weds ramiya");
+//		}
+//		else {
+//			System.out.println("suriya weds megana");
+//		}
+		
+		
+		for(int i=0;i<=3;i++){
+		   for(int j=0;j<=3;j++){
+			   System.out.print ("*");
+		   }
+		   
+		   System.out.println ();
+		   
+		
+		   
+		}
+	}
+
+}
