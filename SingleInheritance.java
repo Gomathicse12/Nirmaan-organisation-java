65df3631f7900a03f808831b8ff41e103f7e3f60
Gomathicse12 <132119074+Gomathicse12@users.noreply.github.com>
2024-12-10 11:12:19 +0530

    day13

---
+++ SingleInheritance.java
@@ -0,0 +1,28 @@
+package day13;
+
+class Animal{
+	void makesound() {
+		System.out.println("sound");
+	}
+}
+ class Cat extends Animal{
+	 @Override
+ void makesound() {
+	 System.out.println("Cat meows");}
+ }
+ class Dog extends Animal{
+	 @Override
+	 void makesound() {
+		 System.out.println("Dog barks ");}
+	 }
+	 
+
+public class SingleInheritance {
+	public static void main(String args[]) {
+		Cat c=new Cat();
+		c.makesound();
+		Dog d=new Dog();
+		d.makesound();
+	} 
+
+}
